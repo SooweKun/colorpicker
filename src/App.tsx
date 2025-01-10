85916bf94fe8168d2955eb6e60e257f7ed48bbea
SooweKun <apextoplega@mail.ru>
2025-01-10 23:30:57 +0300

    +

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ColorPicker } from "/src/components/colorPicker/colorPicker.tsx"
+
+function App() {
+  return (
+    <div className="flex w-full h-screen justify-center items-center">
+      <ColorPicker />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/colorPicker/colorPicker.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from 'react';
+import { RgbaColorPicker } from "react-colorful";
+
+export const ColorPicker = () => {
+    const [rgbaColor, setRgbaColor] = useState({ r: 170, g: 187, b: 204, a: 1 });
+
+    const handleColorChange = (color:any) => {
+        setRgbaColor(color);
+    };
+
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <h2>RGBA Color Picker</h2>
+            <RgbaColorPicker color={rgbaColor} onChange={handleColorChange} />
+            <div
+                style={{
+                    backgroundColor: `rgba(${rgbaColor.r}, ${rgbaColor.g}, ${rgbaColor.b}, ${rgbaColor.a})`,
+                }}
+                className='w-[100px] h-[100px] rounded-3xl mt-7'
+            />
+        </div>
+    );
+}
